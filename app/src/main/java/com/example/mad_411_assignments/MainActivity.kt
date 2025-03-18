2af6f70a5cc39e27309606d6d11df73084348ed2
Gursimran Kaur <W0848501@myscc.ca>
2025-03-18 14:36:42 -0400

    assignment done

---
+++ app/src/main/java/com/example/mad_411_assignments/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mad_411_assignments
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.ceil
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var nameBox = findViewById<EditText>(R.id.nameBox);
+        var resultBox = findViewById<TextView>(R.id.result)
+        var button = findViewById<Button>(R.id.calc_button)
+
+        button.setOnClickListener() {
+            var nameText = nameBox.text
+            resultBox.text = "Hello, $nameText !"
+        }
+    }
+}
